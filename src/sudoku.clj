bf87c2cc185d2c5de7ab4806fc56f0dfb544dcce
Kevin J. Qiu <kevin.jing.qiu@gmail.com>
2011-03-01 23:07:01 -0500

    sudoku namespace is now the only entry point

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -1,2 +1,9 @@
-(ns sudoku)
+(ns sudoku
+  (:use [sudoku.grid :only [render-grid]]
+        [sudoku.solver :only [solve]]))
+
+(def *test-grid* "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
+
+(let [grid (solve *test-grid*)]
+  (println (render-grid @grid)))
 
--- src/sudoku/solver.clj
+++ src/sudoku/solver.clj
@@ -86,7 +86,3 @@
   (let [grid-ref (parse-grid grid-str)]
     (search grid-ref)))
 
-(def *test-grid* "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......")
-(let [grid (solve *test-grid*)]
-  (println (render-grid @grid)))
-
